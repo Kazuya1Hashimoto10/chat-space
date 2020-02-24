0fcec91292fd9087f26293716f86961f16e720b3
Kazuya1Hashimoto10 <kazuya110gtw@icloud.com>
2020-02-24 18:59:51 +0900

    取得した最新のメッセージをブラウザのメッセージ一覧に追加,自動更新をついか

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,25 @@
 $(function(){ 
-  console.log(last_message_id);
-  function buildHTML(message){
-   if ( message.image ) {
+  var buildHTML = function(message){
+    if ( message.content && message.image ) {
+      var html =
+      `<div class="chat-main__message-list--message" data-message-id=${message.id}>
+         <div class="chat-main__message-list--message--up-side">
+           <div class="chat-main__message-list--message--up-side--name">
+             ${message.user_name}
+           </div>
+           <div class="chat-main__message-list--message--up-side--date">
+             ${message.created_at}
+           </div>
+         </div>
+         <div class="chat-main__message-list--message--content">
+           <p class="chat-main__message-list--message--content--text">
+             ${message.content}
+           </p>
+         </div>
+         <img src="` + message.image + `" class="chat-main__message-list--message--content--image" >
+       </div>`
+     return html;
+    } else if (message.content){
      var html =
       `<div class="chat-main__message-list--message" data-message-id=${message.id}>
          <div class="chat-main__message-list--message--up-side">
@@ -17,11 +35,10 @@ $(function(){
              ${message.content}
            </p>
          </div>
-         <img src=${message.image} >
        </div>`
      return html;
-   } else {
-     var html =
+    } else if (message.image){
+      var html =
       `<div class="chat-main__message-list--message" data-message-id=${message.id}>
          <div class="chat-main__message-list--message--up-side">
            <div class="chat-main__message-list--message--up-side--name">
@@ -32,14 +49,12 @@ $(function(){
            </div>
          </div>
          <div class="chat-main__message-list--message--content">
-           <p class="chat-main__message-list--message--content--text">
-             ${message.content}
-           </p>
          </div>
+         <img src="` + message.image + `" class="chat-main__message-list--message--content--image" >
        </div>`
      return html;
-   };
- }
+    };
+  }
   $('#new_message').on('submit', function(e){
   e.preventDefault();
   var formData = new FormData(this);
@@ -76,10 +91,17 @@ $(function(){
       data: {id: last_message_id}
     })
     .done(function(messages) {
-      console.log('success');
+      var insertHTML = '';
+      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      //メッセージが入ったHTMLに、入れ物ごと追加
+      $('.messages').append(insertHTML);
     })
     .fail(function() {
       console.log('error');
     });
   };
+  setInterval(reloadMessages, 7000);
 });
