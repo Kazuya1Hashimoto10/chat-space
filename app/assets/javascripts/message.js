ceb38a429047b71a9c39f5af0d86b102a86c4666
Kazuya1Hashimoto10 <kazuya110gtw@icloud.com>
2020-02-24 14:36:25 +0900

    連続で送信ボタンを押せるようにする

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -56,6 +56,7 @@ $('#new_message').on('submit', function(e){
     $('.chat-main__message-list').append(html);  
     $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});    
     $('form')[0].reset();
+    $('.form-box--submit').prop('disabled', false);
   })
 })
 });
