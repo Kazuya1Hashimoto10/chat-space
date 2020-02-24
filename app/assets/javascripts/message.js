a916be1a2f4e76b881294081c775e58734de29f8
Kazuya1Hashimoto10 <kazuya110gtw@icloud.com>
2020-02-24 14:29:32 +0900

    作成したHTMLをメッセージ画面の一番下に追加する

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,17 +1,60 @@
-$(function(){
-
-  $('#new_message').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action');
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-  });
-
+$(function(){ 
+  function buildHTML(message){
+   if ( message.image ) {
+     var html =
+      `<div class="chat-main__message-list--message" data-message-id=${message.id}>
+         <div class="chat-main__message-list--message--up-side">
+           <div class="chat-main__message-list--message--up-side--name">
+             ${message.user_name}
+           </div>
+           <div class="chat-main__message-list--message--up-side--date">
+             ${message.date}
+           </div>
+         </div>
+         <div class="chat-main__message-list--message--content">
+           <p class="chat-main__message-list--message--content--text">
+             ${message.content}
+           </p>
+         </div>
+         <img src=${message.image} >
+       </div>`
+     return html;
+   } else {
+     var html =
+      `<div class="chat-main__message-list--message" data-message-id=${message.id}>
+         <div class="chat-main__message-list--message--up-side">
+           <div class="chat-main__message-list--message--up-side--name">
+             ${message.user_name}
+           </div>
+           <div class="chat-main__message-list--message--up-side--date">
+             ${message.date}
+           </div>
+         </div>
+         <div class="chat-main__message-list--message--content">
+           <p class="chat-main__message-list--message--content--text">
+             ${message.content}
+           </p>
+         </div>
+       </div>`
+     return html;
+   };
+ }
+$('#new_message').on('submit', function(e){
+ e.preventDefault();
+ var formData = new FormData(this);
+ var url = $(this).attr('action')
+ $.ajax({
+   url: url,
+   type: "POST",
+   data: formData,
+   dataType: 'json',
+   processData: false,
+   contentType: false
+ })
+  .done(function(data){
+    var html = buildHTML(data);
+    $('.chat-main__message-list').append(html);      
+    $('form')[0].reset();
+  })
+})
 });
